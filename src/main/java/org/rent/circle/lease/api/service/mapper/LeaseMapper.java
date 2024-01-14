0dce71167af4de7d081afbb49fd5c8de4b3ea83b
matos-nelson <nmatos6@gatech.edu>
2024-01-14 16:40:03 -0600

    Implemented toDto mapping functionality.

--- src/main/java/org/rent/circle/lease/api/service/mapper/LeaseMapper.java
+++ src/main/java/org/rent/circle/lease/api/service/mapper/LeaseMapper.java
@@ -3,10 +3,13 @@ package org.rent.circle.lease.api.service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.rent.circle.lease.api.dto.CreateLeaseDto;
+import org.rent.circle.lease.api.dto.LeaseDto;
 import org.rent.circle.lease.api.persistence.model.Lease;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "cdi")
 public interface LeaseMapper {
 
     Lease toModel(CreateLeaseDto createLease);
+
+    LeaseDto toDto(Lease lease);
 }
--- src/test/java/org/rent/circle/lease/api/service/mapper/LeaseMapperTest.java
+++ src/test/java/org/rent/circle/lease/api/service/mapper/LeaseMapperTest.java
@@ -11,14 +11,15 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.rent.circle.lease.api.dto.CreateLeaseDto;
 import org.rent.circle.lease.api.dto.CreateTenantDto;
+import org.rent.circle.lease.api.dto.LeaseDto;
 import org.rent.circle.lease.api.persistence.model.Lease;
+import org.rent.circle.lease.api.persistence.model.Tenant;
 
 @QuarkusTest
 public class LeaseMapperTest {
 
     @Inject
     LeaseMapper leaseMapper;
-
 
     @Test
     public void toModel_WhenGivenNull_ShouldReturnNull() {
@@ -89,5 +90,66 @@ public class LeaseMapperTest {
         assertEquals(createLeaseDto.getPetDepositHeld(), result.getPetDepositHeld());
         assertEquals(createTenantDto.getResidentId(), result.getTenants().get(0).getResidentId());
         assertEquals(createTenantDto.isPrimary(), result.getTenants().get(0).isPrimary());
+    }
+
+    @Test
+    public void toDto_WhenGivenNull_ShouldReturnNull() {
+        // Arrange
+
+        // Act
+        LeaseDto result = leaseMapper.toDto(null);
+
+        // Assert
+        assertNull(result);
+    }
+
+    @Test
+    public void toDto_WhenGivenAValidLease_ShouldMap() {
+        // Arrange
+        Lease lease = new Lease();
+        lease.setId(1L);
+        lease.setManagerId("abc123");
+        lease.setOwnerId(2L);
+        lease.setPropertyId(3L);
+        lease.setStartDate(LocalDate.now());
+        lease.setEndDate(LocalDate.now());
+        lease.setMonthlyRent(BigDecimal.ONE);
+        lease.setSecurityDepositHeld(BigDecimal.ZERO);
+        lease.setPetDepositHeld(BigDecimal.TEN);
+
+        // Act
+        LeaseDto result = leaseMapper.toDto(lease);
+
+        // Assert
+        assertEquals(lease.getId(), result.getId());
+        assertEquals(lease.getOwnerId(), result.getOwnerId());
+        assertEquals(lease.getPropertyId(), result.getPropertyId());
+        assertEquals(lease.getStartDate(), result.getStartDate());
+        assertEquals(lease.getEndDate(), result.getEndDate());
+        assertEquals(lease.getMonthlyRent(), result.getMonthlyRent());
+        assertEquals(lease.getSecurityDepositHeld(), result.getSecurityDepositHeld());
+        assertEquals(lease.getPetDepositHeld(), result.getPetDepositHeld());
+        assertNull(result.getTenants());
+    }
+
+    @Test
+    public void toDto_WhenGivenAValidLeaseWithTenants_ShouldMap() {
+        // Arrange
+        Lease lease = new Lease();
+        lease.setId(1L);
+
+        Tenant tenant = new Tenant();
+        tenant.setId(5L);
+        tenant.setLeaseId(lease.getId());
+        tenant.setPrimary(true);
+
+        lease.setTenants(Collections.singletonList(tenant));
+
+        // Act
+        LeaseDto result = leaseMapper.toDto(lease);
+
+        // Assert
+        assertEquals(tenant.getResidentId(), result.getTenants().get(0).getResidentId());
+        assertEquals(tenant.isPrimary(), result.getTenants().get(0).isPrimary());
     }
 }
