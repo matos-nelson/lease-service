c4846487dbb61cf8d54d414c81e40ee82696c97b
matos-nelson <nmatos6@gatech.edu>
2024-01-14 16:48:24 -0600

    Implemented getLease service functionality.

--- src/main/java/org/rent/circle/lease/api/service/LeaseService.java
+++ src/main/java/org/rent/circle/lease/api/service/LeaseService.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.rent.circle.lease.api.dto.CreateLeaseDto;
+import org.rent.circle.lease.api.dto.LeaseDto;
 import org.rent.circle.lease.api.persistence.model.Lease;
 import org.rent.circle.lease.api.persistence.repository.LeaseRepository;
 import org.rent.circle.lease.api.service.mapper.LeaseMapper;
@@ -24,5 +25,15 @@ public class LeaseService {
 
         leaseRepository.persist(lease);
         return lease.getId();
+    }
+
+    public LeaseDto getLease(Long leaseId, String managerId) {
+        Lease lease = leaseRepository.findByIdAndManagerId(leaseId, managerId);
+        if (lease == null) {
+            log.info("Could Not Find Lease With Id: {}", leaseId);
+            return null;
+        }
+
+        return leaseMapper.toDto(lease);
     }
 }
--- src/test/java/org/rent/circle/lease/api/service/LeaseServiceTest.java
+++ src/test/java/org/rent/circle/lease/api/service/LeaseServiceTest.java
@@ -1,6 +1,7 @@
 package org.rent.circle.lease.api.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,6 +11,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.rent.circle.lease.api.dto.CreateLeaseDto;
+import org.rent.circle.lease.api.dto.LeaseDto;
 import org.rent.circle.lease.api.persistence.model.Lease;
 import org.rent.circle.lease.api.persistence.repository.LeaseRepository;
 import org.rent.circle.lease.api.service.mapper.LeaseMapper;
@@ -44,5 +46,44 @@ public class LeaseServiceTest {
         // Assert
         assertEquals(lease.getId(), result);
         verify(leaseRepository, times(1)).persist(lease);
+    }
+
+    @Test
+    public void getLease_WhenLeaseCantBeFound_ShouldReturnNull() {
+        // Arrange
+        Long leaseId = 100L;
+        String managerId = "abc123";
+
+        when(leaseRepository.findByIdAndManagerId(leaseId, managerId)).thenReturn(null);
+
+        // Act
+        LeaseDto result = leaseService.getLease(leaseId, managerId);
+
+        // Assert
+        assertNull(result);
+    }
+
+    @Test
+    public void getLease_WhenCalled_ShouldReturnLease() {
+        // Arrange
+        Long leaseId = 100L;
+        String managerId = "abc123";
+
+        Lease lease = new Lease();
+        lease.setId(100L);
+        lease.setManagerId(managerId);
+
+        LeaseDto leaseDto = LeaseDto.builder()
+            .id(lease.getId())
+            .build();
+
+        when(leaseRepository.findByIdAndManagerId(leaseId, managerId)).thenReturn(lease);
+        when(leaseMapper.toDto(lease)).thenReturn(leaseDto);
+
+        // Act
+        LeaseDto result = leaseService.getLease(leaseId, managerId);
+
+        // Assert
+        assertEquals(leaseDto, result);
     }
 }
